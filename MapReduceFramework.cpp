97aa5ecdd7827df7cb8046ffb2385bc8a60c63b0
halden718 <attiaa13@gmail.com>
2016-05-07 09:25:24 +0300

    add

---
+++ MapReduceFramework.cpp
@@ -0,0 +1,7 @@
+#include "MapReduceFramework.h"
+
+OUT_ITEMS_LIST runMapReduceFramework(MapReduceBase& mapReduce, IN_ITEMS_LIST& itemsList, int multiThreadLevel);
+
+void Emit2 (k2Base*, v2Base*);
+
+void Emit3 (k3Base*, v3Base*);
